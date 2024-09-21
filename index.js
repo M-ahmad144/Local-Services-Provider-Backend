f7ad5ef0602e17c3fc4679d2d930dd469917471f
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2024-09-22 00:14:35 +0500

    Backend setup

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+const { parse } = require('pg-connection-string');
+
+dotenv.config();
+
+
+const config = parse(process.env.DATABASE_URL);
+
+
+config.ssl = {
+  rejectUnauthorized: false,
+};
+
+
+const pool = new Pool(config);
+
+// Test the connection
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+  } else {
+    console.log('Connected to the database');
+    release();
+  }
+});
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+// TEST CODE RUN THIS AND DONT REMOVE IT
+app.get('/test', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM playing_with_neon;');
+    const users = result.rows;
+    client.release();
+    res.json(users);
+  } catch (err) {
+    console.error('Error fetching users:', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
