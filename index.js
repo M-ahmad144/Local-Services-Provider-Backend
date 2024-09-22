de4f0a59b1892c3bbfe70bd7d333ca1ca8b83fc8
Robass Atif <mohammadrobass@gmail.com>
2024-09-22 15:37:40 +0500

    fix error

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 const { parse } = require('pg-connection-string');
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
+
 dotenv.config();
 
 
