3706b135c97b3d6ee837547ff63d13293825295d
Robass Atif <mohammadrobass@gmail.com>
2024-09-22 17:46:58 +0500

    deploy

--- index.js
+++ index.js
@@ -3,23 +3,19 @@ const { PrismaClient } = require('@prisma/client');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { parse } = require('pg-connection-string');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const { request } = require('express');
 
 const serviceProviderRouter = require('./Routes/ServiceProvider')
 
 dotenv.config();
 
 const prisma = new PrismaClient();
-
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.use(serviceProviderRouter)
+app.use(serviceProviderRouter);
+
 const port = process.env.PORT || 3000;
 
 // Connect to the database
@@ -27,16 +23,14 @@ async function config() {
   try {
     await prisma.$connect();
     console.log('Connected to the database');
-    
-    
     app.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`);
     });
   } catch (err) {
     console.error('Error connecting to the database:', err);
+    process.exit(1); // Exit if connection fails
   }
 }
-
 
 app.get('/test', async (req, res) => {
   try {
@@ -47,6 +41,5 @@ app.get('/test', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
-
 
 config();
