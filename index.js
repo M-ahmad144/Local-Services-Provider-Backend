4eb79c731ebc85760c44ded1dfdbe86b61f90689
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2024-10-07 22:07:53 +0500

    CORS issue

--- index.js
+++ index.js
@@ -25,15 +25,23 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-    origin: ['https://myneighbourly.vercel.app/', 'http://localhost:5173/'], // List of allowed origins
-  };
+  origin: ['https://myneighbourly.vercel.app', 'http://localhost:5173'], // List of allowed origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true, // Enable credentials (if needed)
+};
+
+
+
 
   
 // Middleware setup
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(cors(corsOptions));
+
 
 // Set up routes
 app.use("/serviceProvider", serviceProviderRouter);
