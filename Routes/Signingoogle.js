c362f239515fe67b3794c6259cacf668155dd1c3
Robass Atif <mohammadrobass@gmail.com>
2024-10-11 15:59:54 +0500

    error

--- Routes/Signingoogle.js
+++ Routes/Signingoogle.js
@@ -52,9 +52,9 @@ router.post('/', async (req, res) => {
       console.log('User created:', user);
     } else {
       verifyuser=await User.findOneAndUpdate({email},{verify:true})
+
       
-      verifyuser.save();
-      verifyuser.verify=true;
+      
       console.log('User already exists:', verifyuser);
     }
 
