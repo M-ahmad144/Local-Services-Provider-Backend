f51731aa7b62926b70cf4970ad9184da5168d32c
Muhammad Ahmad <muhammadahmadmughal0987@gmail.com>
2024-11-23 15:47:24 +0500

    update profile

--- Controllers/Profile.js
+++ Controllers/Profile.js
@@ -1,68 +1,64 @@
-const User = require('../Models/User');
-
-
-
+const User = require("../Models/User");
 
 const GetProfileById = async (req, res) => {
-    const user_id = req.params.user_id;
+  const user_id = req.params.user_id;
 
-    try {
-        // Find the user by ID
-        const userProfile = await User.findById(user_id);
+  try {
+    // Find the user by ID
+    const userProfile = await User.findById(user_id);
 
-        // If no user is found, return a 404 error
-        if (!userProfile) {
-            return res.status(404).json({ error: "User not found." });
-        }
-
-        // If user is found, return the profile data
-        res.status(200).json(userProfile);
-    } catch (error) {
-        console.error("Error retrieving profile:", error);
-        res.status(500).json({ error: "An error occurred while retrieving the profile." });
+    // If no user is found, return a 404 error
+    if (!userProfile) {
+      return res.status(404).json({ error: "User not found." });
     }
+
+    // If user is found, return the profile data
+    res.status(200).json(userProfile);
+  } catch (error) {
+    console.error("Error retrieving profile:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while retrieving the profile." });
+  }
 };
-
-
-
 
 const UpdateProfile = async (req, res) => {
-    const data = req.body;
-    const user_id = req.params.user_id;
+  const data = req.body;
+  const user_id = req.params.user_id;
 
-    try {
+  try {
+    const updateFields = {
+      ...(data.name && { name: data.name }),
+      ...(data.location && { location: data.location }),
+      ...(data.profileDescription && {
+        profile_description: data.profileDescription,
+      }),
+      ...(data.profile_image && { profile_image: data.profile_image }),
+      ...(data.skills && { skills: data.skills }),
+      ...(data.languages && { language: data.languages }),
+      updated_at: new Date(),
+    };
 
-        const updateFields = {
-            name: data.name,
-            location: data.location,
-            profile_description: data.profileDescription,
-            profile_image: data.profile_image,
-            skills: data.skills,
-            language: data.languages,
-            updated_at: new Date()
-        };
-        
+    const updatedProfile = await User.findOneAndUpdate(
+      { _id: user_id },
+      { $set: updateFields },
+      { new: true, upsert: false }
+    );
 
-        const updatedProfile = await User.findOneAndUpdate(
-            { _id: user_id },
-            { $set: updateFields }, 
-            { new: true, upsert: false } 
-        );
-
-        if (!updatedProfile) {
-            return res.status(404).json({ error: "User not found." });
-        }
-
-
-        res.status(200).json(updatedProfile);
-    } catch (error) {
-        console.error("Error updating profile:", error);
-        res.status(500).json({ error: "An error occurred while updating the profile." });
+    if (!updatedProfile) {
+      return res.status(404).json({ error: "User not found." });
     }
+
+    res.status(200).json(updatedProfile);
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while updating the profile." });
+  }
 };
 
-
 module.exports = {
-    UpdateProfile,
-    GetProfileById
+  UpdateProfile,
+  GetProfileById,
 };
--- index.js
+++ index.js
@@ -13,22 +13,15 @@ const chatRoutes = require("./Routes/chatRoutes");
 const messageRoutes = require("./Routes/messagesRoutes");
 const checkoutRoutes = require("./Routes/stripeCheckout");
 const analyticsRoutes = require("./Routes/AnalyticsRoutes");
-
-
 // Global error handler middleware
 const globalErrorHandler = require("./middlewares/globalErrorHandler");
 
 dotenv.config();
 
 const app = express();
-
 // CORS options
 const corsOptions = {
-  origin: [
-    "https://myneighbourly.vercel.app",
-    "http://localhost:5173",
-    "http://localhost:5173",
-  ],
+  origin: ["https://myneighbourly.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Enable credentials
@@ -39,7 +32,6 @@ app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
-
 // Set up routes
 app.use("/serviceProvider", serviceProviderRouter);
 app.use("/profile", profileRouter);
@@ -50,8 +42,6 @@ app.use("/chat", chatRoutes);
 app.use("/messages", messageRoutes);
 app.use("/payments", checkoutRoutes);
 app.use("/analytics", analyticsRoutes);
-
-
 // Global error handler
 app.use(globalErrorHandler);
 
