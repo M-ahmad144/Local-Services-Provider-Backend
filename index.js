80e4256549b0bc851ea546e0eab7ca33de22c640
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2024-12-11 18:58:01 +0500

    CORS fix

--- index.js
+++ index.js
@@ -31,6 +31,15 @@ const corsOptions = {
   credentials: true, // Enable credentials
 };
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Or specify your domains
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 app.use(express.json());
